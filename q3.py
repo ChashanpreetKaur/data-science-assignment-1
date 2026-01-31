ff629a174ac00175cd50d0d362a40b05000379a8
ChashanpreetKaur <chashan.preetkaur597@gmail.com>
2026-01-30 20:19:54 -0700

    Create q3.py

---
+++ q3.py
@@ -0,0 +1,10 @@
+def power(x,y):
+  return x ** y
+pairs = [[5,2], [3, -1], [4,3], [2,0]]
+results = []
+
+for x, y in pairs:
+  if y < 0:
+    continue
+  results.append(power(x,y))
+print(results)
